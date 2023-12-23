b82a4f6d195aa2d3f10a33edcdbcd6f6abd15b76
Scott Lucchini <scott.lucchini@gmail.com>
2023-12-23 15:26:46 -0500

    Day 1 update (not complete)

--- Day1.py
+++ Day1.py
@@ -1,15 +1,42 @@
-import numpy as np
+
+def getNumsFromLines(lines):
+    nums = []
+    for line in lines:
+        linenums = []
+        for char in line:
+            try:
+                linenums.append(int(char))
+            except:
+                None
+        nums.append(linenums[0]*10+linenums[-1])
+    return nums
+
+def sumNumArray(nums):
+    tot = 0
+    for n in nums:
+        tot += n
+    return tot
 
 f = open("data/Day1.dat")
+all_lines = f.readlines()
 
-nums = []
-for line in f:
-    linenums = []
-    for char in line:
-        try:
-            linenums.append(int(char))
-        except:
-            None
-    nums.append(linenums[0]*10+linenums[-1])
+nums = getNumsFromLines(all_lines)
+tot = sumNumArray(nums)
+print("Part 1: {}".format(tot))
 
-print(np.sum(nums))
+convertedLines = []
+textnumbers = ['one','two','three','four','five','six','seven','eight','nine']
+for line in all_lines:
+    cl = line
+    for i in range(len(cl)-1):
+        for j in range(len(textnumbers)):
+            if (cl[i:].startswith(textnumbers[j])):
+                if (len(cl[i:]) == len(textnumbers[j])):
+                    cl = cl[:i]+str(j+1)
+                else:
+                    cl = cl[:i]+str(j+1)+cl[i+len(textnumbers[j]):]
+    convertedLines.append(cl)
+
+nums = getNumsFromLines(convertedLines)
+tot = sumNumArray(nums)
+print("Part 2: {}".format(tot))
