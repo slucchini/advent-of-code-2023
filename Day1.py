b9f5b8cd651d6caf9546925eb090fb217184e4c2
Scott Lucchini <scott.lucchini@gmail.com>
2023-12-29 21:57:49 -0500

    Day 1 completed.

--- Day1.py
+++ Day1.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+def isNum(s):
+    try:
+        n = int(s)
+    except:
+        return False
+    return True
 
 def getNumsFromLines(lines):
     nums = []
@@ -28,15 +36,30 @@ convertedLines = []
 textnumbers = ['one','two','three','four','five','six','seven','eight','nine']
 for line in all_lines:
     cl = line
-    for i in range(len(cl)-1):
-        for j in range(len(textnumbers)):
-            if (cl[i:].startswith(textnumbers[j])):
-                if (len(cl[i:]) == len(textnumbers[j])):
-                    cl = cl[:i]+str(j+1)
-                else:
-                    cl = cl[:i]+str(j+1)+cl[i+len(textnumbers[j]):]
+    swaps = []
+    for j in range(len(textnumbers)):
+        indices = [i for i in range(len(cl)) if cl.startswith(textnumbers[j], i)]
+        for i in indices:
+            swaps.append([i,len(textnumbers[j]),j+1])
+    swaps = np.array(swaps)
+    for s in swaps:
+        cl = cl[:s[0]]+str(s[2])+cl[s[0]+1:]
+    for si in range(len(swaps)):
+        s = swaps[si]
+        for k in range(s[1]-1):
+            if isNum(cl[s[0]+1]):
+                break
+            cl = cl[:s[0]+1]+cl[s[0]+2:]
+            shiftmask = swaps[:,0] > s[0]
+            swaps[shiftmask,0] -= 1
     convertedLines.append(cl)
+
 
 nums = getNumsFromLines(convertedLines)
 tot = sumNumArray(nums)
 print("Part 2: {}".format(tot))
+
+f2 = open("data/Day1_test.dat",'w')
+for i in range(len(convertedLines)):
+    f2.write(convertedLines[i][:-1]+'  :  {}\n'.format(nums[i]))
+f2.close()
